cda14bd63ee21ab0725d9b22b6a67cc13447ae56
Evan Sims <evan@ushahidi.com>
2012-08-08 22:05:08 -0500

    Updated description.

--- tweetbot.user.js
+++ tweetbot.user.js
@@ -1,11 +1,11 @@
 // ==UserScript==
 // @name		   Tweetbot instead of Twitter
-// @description	   Open Tweetbot instead of loading Twitter.com
+// @description	   	   Open Tweetbot when you load Twitter.com.
 // @match		   http://twitter.com/
 // @match		   https://twitter.com/
 // @require		   http://userscripts.org/scripts/source/49700.user.js
 // @require		   http://userscripts.org/scripts/source/50018.user.js
-// @version		   0.0.2
+// @version		   0.0.1
 // ==/UserScript==
 
 function main() {
