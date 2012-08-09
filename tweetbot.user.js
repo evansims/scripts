ff4c2db9e7f7b948401304ab6e6668d421b19a30
Evan Sims <evansims@gmail.com>
2012-08-08 22:52:26 -0500

    Support for /usernames

--- tweetbot.user.js
+++ tweetbot.user.js
@@ -1,21 +1,29 @@
 // ==UserScript==
-// @name		   Replace Twitter.com with Tweetbot
-// @description	   Open Tweetbot instead of loading Twitter.com
-// @match		   http://twitter.com/*
-// @match		   https://twitter.com/*
-// @require		   http://userscripts.org/scripts/source/49700.user.js
-// @require		   http://userscripts.org/scripts/source/50018.user.js
-// @version		   0.0.3
+// @name           Replace Twitter.com with Tweetbot
+// @description    Open Tweetbot instead of loading Twitter.com
+// @match          http://twitter.com/*
+// @match          https://twitter.com/*
+// @require        http://userscripts.org/scripts/source/49700.user.js
+// @require        http://userscripts.org/scripts/source/50018.user.js
+// @version        0.0.4
 // ==/UserScript==
 
 function main() {
 
-	window.addEventListener("load", function() {
-		var tweetbot = document.createElement('iframe');
-			tweetbot.src = 'tweetbot:///';
+    window.addEventListener("load", function() {
+        var tweetbot = document.createElement('iframe');
+            tweetbot.src = 'tweetbot://';
 
-		document.body.appendChild(tweetbot);
-	});
+        var path = location.pathname.substr(1);
+
+        if(path.length) {
+            tweetbot.src += path;
+        } else {
+            tweetbot.src += '/';
+        }
+
+        document.body.appendChild(tweetbot);
+    });
 
 }
 
